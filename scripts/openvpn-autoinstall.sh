d438fc1c9983c7a136dbf7d12e7dd06a23504fcb
Viorel Anghel <vang@hq.ro>
2017-07-12 16:47:00 +0300

    updated for new easyrsa location

--- scripts/openvpn-autoinstall.sh
+++ scripts/openvpn-autoinstall.sh
@@ -69,6 +69,8 @@ CLIENT=aws_vpn
 if [[ "$OS" = 'debian' ]]; then
 	apt-get update
 	apt-get install openvpn iptables openssl ca-certificates -y
+# install some more tools i consider to be useful
+        apt-get install fail2ban haveged ntp unzip
 else
 	# distro is CentOS
 	yum install epel-release -y
@@ -81,13 +83,14 @@ if [[ -d /etc/openvpn/easy-rsa/ ]]; then
 fi
 
 # Get easy-rsa
-wget -O ~/EasyRSA-3.0.1.tgz https://github.com/OpenVPN/easy-rsa/releases/download/3.0.1/EasyRSA-3.0.1.tgz
-tar xzf ~/EasyRSA-3.0.1.tgz -C ~/
-mv ~/EasyRSA-3.0.1/ /etc/openvpn/
-mv /etc/openvpn/EasyRSA-3.0.1/ /etc/openvpn/easy-rsa/
+[ -f easy-rsa.zip ] || wget -O easy-rsa.zip https://github.com/OpenVPN/easy-rsa/archive/master.zip
+[ -d easy-rsa-master ] || unzip easy-rsa.zip
+cp -a easy-rsa-master/easyrsa3 /etc/openvpn/
+mv /etc/openvpn/easyrsa3  /etc/openvpn/easy-rsa
+#rm easy-rsa.zip
 chown -R root:root /etc/openvpn/easy-rsa/
-rm -rf ~/EasyRSA-3.0.1.tgz
 cd /etc/openvpn/easy-rsa/
+
 # Create the PKI, set up the CA, the DH params and the server + client certificates
 ./easyrsa init-pki
 ./easyrsa --batch build-ca nopass
